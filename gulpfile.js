758c1ee187ff8f4935310a4343b3e59a2aec2d78
Matt Lindsay <matt_epilp@hotmail.com>
2018-01-19 10:12:46 +0000

    Project setup.

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const gulp = require('gulp');
+const jshint = require('gulp-jshint');
+const jscs = require('gulp-jscs');
+const nodemon = require('gulp-nodemon');
+
+var jsFiles = ['*.js', 'src/**/*.js'];
+
+gulp.task('style', function() {
+    return gulp.src(jsFiles)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {
+            verbose: true
+          }))
+        .pipe(jscs());
+  });
+
+gulp.task('serve', ['style'], function() {
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: { 'PORT': process.env.PORT || 3000 },
+        watch: jsFiles
+      };
+    return nodemon(options)
+        .on('restart', function(ev) {
+            console.log('Restarting....' + ev);
+          });
+  });
