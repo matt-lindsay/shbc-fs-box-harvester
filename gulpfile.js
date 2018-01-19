87e4b7e609aa0dbeaceb029b8d1df069cd802f24
Matt Lindsay <matt_epilp@hotmail.com>
2018-01-19 10:35:38 +0000

    Added initial dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const gulp = require('gulp');
+const env = require('gulp-env');
 const jshint = require('gulp-jshint');
 const jscs = require('gulp-jscs');
+const gulpMocha = require('gulp-mocha');
 const nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
@@ -14,6 +16,12 @@ gulp.task('style', function() {
             verbose: true
           }))
         .pipe(jscs());
+  });
+
+gulp.task('test', function () {
+    env({ vars: { ENV: 'Test' }});
+    gulp.src('tests/*.js', { read: false })
+      .pipe(gulpMocha({ report: 'nyan' }));
   });
 
 gulp.task('serve', ['style'], function() {
